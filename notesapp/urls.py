d925c7790a5c4c2c1a3828fc361afdcfba5f2dd3
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2024-08-28 22:47:05 +0530

    adding features to delete and get notes

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("notes/", views.notes, name="notes")
+    path("notes/", views.notes, name="notes"),
+    path("notes/<slug:slug>", views.note_detail, name="note-detail"),
 ]
--- notesapp/views.py
+++ notesapp/views.py
@@ -22,3 +22,25 @@ def notes(request):
             serializer.save()
             return Response(serializer.data , status=status.HTTP_201_CREATED)
         return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def note_detail(request, slug):
+    try:
+        note = Note.objects.get(slug= slug)
+    except Note.DoesNotExist:
+        return Response(status= status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = NoteSerializer(note, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == "DELETE":
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
