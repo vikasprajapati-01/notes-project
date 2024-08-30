40056ae76e0a07e9ee33724013f4f2348e158167
vikasprajapati-01 <vikasprajapati9980@gmail.com>
2024-08-30 22:17:55 +0530

    checking api

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path("notes/", views.notes, name="notes"),
     path("notes/<slug:slug>", views.note_detail, name="note-detail"),
 ]
+
+# To access notes data
+# To get all the notes : HOSTNUMBER/notes
+# To get a particular note : HOSTNUMBER/notes/note-slug
